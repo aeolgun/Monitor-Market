6abb43f2e2c5713969a7ff8c7d9fb5bae629f64a
aeolgun <42916536+aeolgun@users.noreply.github.com>
2026-03-31 04:49:24 +0300

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -1,19 +1,9 @@
-
-# Hareketli Ortalama Ayarları
 EMA_HIZLI = 20
 EMA_YAVAS = 50
-
-# RSI Ayarları
 RSI_PERIYOT = 14
 RSI_SINIR = 50
 RSI_ASIRI_ALIM = 75
-
-# Hacim Ayarları
 HACIM_MA_PERIYOT = 20
-
-# Risk Yönetimi (Yüzde %)
-KAR_AL_ORAN = 25.0    # %25 kâr hedefi
-ZARAR_KES_ORAN = 5.0  # %5 zarar kes (stop loss)
-
-# Uygulama Ayarları
+KAR_AL_ORAN = 25.0    # Eksik olan satır bu olabilir
+ZARAR_KES_ORAN = 5.0  # Eksik olan satır bu olabilir
 GUNCELLEME_SANIYESI = 60
